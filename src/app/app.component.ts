5ccfafc7f532cf12a2d5904e0099056047506f3c
ALICE BERTHELOT <aliceberthelot.pro@gmail.com>
2025-10-25 18:25:35 +0200

    added homepage + started style update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { CoursesComponent } from './courses/courses.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CoursesComponent, ArticlesComponent, HeaderComponent, FooterComponent], 
+  imports: [RouterOutlet, CoursesComponent, ArticlesComponent, HeaderComponent, FooterComponent, HomeComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { CoursesComponent } from './courses/courses.component';
 import { ArticlesComponent } from './articles/articles.component';
 import { ArticleDetailsComponent } from './article-details/article-details.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    { path: '', component: HomeComponent },
     { path: 'articles/all', component: ArticlesComponent },
     { path: 'articles/:id', component: ArticleDetailsComponent },
     { path: 'courses', component: CoursesComponent }
